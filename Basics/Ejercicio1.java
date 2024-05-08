3806feb928cab2e417ac51b038399ad0380575a7
jccarrey <juankazocato634@gmail.com>
2024-05-08 16:13:02 +0200

    Adding first basic excercise

---
+++ Basics/Ejercicio1.java
@@ -0,0 +1,36 @@
+
+package ejercicio.pkg1;
+
+
+public class Ejercicio1 {
+
+  
+    public static void main(String[] args) {
+        
+        byte age;
+        age = 12;
+        System.out.println("My age is" + age + "años");
+        
+        float temperature;
+        temperature = 137.12f;
+        System.out.println("The temperature is" + temperature);
+        
+        double salary;
+        salary = 264368.20;
+        System.out.println("My salary is" + salary);
+       
+        boolean rainy; 
+        rainy = false;
+        System.out.println(rainy);
+        
+        char letter;
+        letter = '@';
+        System.out.println("My letter is" + letter);
+        
+        String greating;
+        greating = " Hallo!! ";
+        System.out.println(greating);
+        
+    }
+    
+}
